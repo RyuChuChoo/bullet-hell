af73ca13cf20f7c6bed10b5da5f3bec625a65625
unknown <asenglar@gmail.com>
2024-06-08 16:01:58 -0400

    adding bullets this sucked so bad I hated it so much omg RAAAAAAAAA

--- scripts/watergun.gd
+++ scripts/watergun.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
+@onready var shooting_point = get_node("WeaponPivot/Watergun/ShootingPoint")
+
 var Can_shoot = true
 
 func shoot():
 	if Can_shoot == true:
 		const BULLET = preload("res://scenes/bullet.tscn")
 		var new_bullet = BULLET.instantiate()
-		new_bullet.global_position = %Watergun/ShootingPoint.global_position
-		%Watergun/ShootingPoint.add_child(new_bullet)
+		new_bullet.global_position = shooting_point.global_position
+		new_bullet.global_rotation = shooting_point.global_rotation
+		shooting_point.add_child(new_bullet)
 		Can_shoot = false
 
 func _input(event):
