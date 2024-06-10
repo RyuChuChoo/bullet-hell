44cd3926a1bd4911519ca1e1c0c7b42a82e871fb
Hat-Boi <eu.tou.aqui.15@gmail.com>
2024-06-10 23:47:26 +0100

    jump done fixed hp fixed borders

--- scripts/crab_movement.gd
+++ scripts/crab_movement.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer
 
 var health = 1
+var jumped = false
+var jump_position = Vector2.ZERO
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
@@ -14,7 +16,19 @@ func _physics_process(delta):
 func take_damage():
 	health -= 1
 	
-	if health == 0: 
+	if health <= 0: 
 		game_manager.add_point()
 		animation_player.play("death")
 
+func jump(area):
+	if area == "leftright" and not jumped:
+		jumped = true
+		animation_player.play("jump")
+
+	elif area == "topbottom" and not jumped:
+		jumped = true
+		animation_player.play("jump")
+
+func splash():
+	if jumped:
+		animation_player.play("splash")
